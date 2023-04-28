9bed6aa5ac61a2e58b70f91aa41d7315a42c6bce
LucasPilot1 <131932050+LucasPilot1@users.noreply.github.com>
2023-04-27 21:07:57 -0500

    Update lista.js

--- lista.js
+++ lista.js
@@ -6,65 +6,80 @@ const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
 const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
 let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 
-
 function cargarProductosCarrito() {
-    if (productosEnCarrito) {
+  if (productosEnCarrito) {
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.remove("disabled");
+    contenedorCarritoAcciones.classList.remove("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
 
-        contenedorCarritoVacio.classList.add("disabled");
-        contenedorCarritoProductos.classList.remove("disabled");
-        contenedorCarritoAcciones.classList.remove("disabled");
-        contenedorCarritoComprado.classList.add("disabled");
+    contenedorCarritoProductos.innerHTML = "";
 
-        contenedorCarritoProductos.innerHTML = "";
+    const categorias = {};
 
-        const categorias = {};
+    productosEnCarrito.forEach(producto => {
+      if (!categorias[producto.categoria.nombre]) {
+        categorias[producto.categoria.nombre] = [];
+      }
+      categorias[producto.categoria.nombre].push(producto);
+    });
 
-        productosEnCarrito.forEach(producto => {
-            if (!categorias[producto.categoria.nombre]) {
-                categorias[producto.categoria.nombre] = [];
-            }
-            categorias[producto.categoria.nombre].push(producto);
-        });
+    let carritoTexto = '';
+    for (const categoria in categorias) {
+      const productosCategoria = categorias[categoria];
+      carritoTexto += `${categoria}\n`;
+      productosCategoria.forEach(producto => {
+        carritoTexto += `- ${producto.titulo}\n`;
+      });
+      carritoTexto += '\n';
 
-        for (const categoria in categorias) {
-            const productosCategoria = categorias[categoria];
-            const divCategoria = document.createElement("div");
-            divCategoria.classList.add("carrito-categoria");
-            divCategoria.innerHTML = `
-                <h2>${categoria}</h2>
-            `;
-            contenedorCarritoProductos.append(divCategoria);
+      const divCategoria = document.createElement("div");
+      divCategoria.classList.add("carrito-categoria");
+      divCategoria.innerHTML = `
+        <h2>${categoria}</h2>
+      `;
+      contenedorCarritoProductos.append(divCategoria);
 
-            productosCategoria.forEach(producto => {
-                const divProducto = document.createElement("div");
-                divProducto.classList.add("carrito-producto");
-                divProducto.innerHTML = `
-                    <div class="carrito-producto-titulo">
-                        <h3>- ${producto.titulo}</h3>
-                    </div>
-                `;
-                divCategoria.append(divProducto);
-            });
-        }
-
-    } else {
-        contenedorCarritoVacio.classList.remove("disabled");
-        contenedorCarritoProductos.classList.add("disabled");
-        contenedorCarritoAcciones.classList.add("disabled");
-        contenedorCarritoComprado.classList.add("disabled");
+      productosCategoria.forEach(producto => {
+        const divProducto = document.createElement("div");
+        divProducto.classList.add("carrito-producto");
+        divProducto.innerHTML = `
+          <div class="carrito-producto-titulo">
+            <h3>- ${producto.titulo}</h3>
+          </div>
+        `;
+        divCategoria.append(divProducto);
+      });
     }
 
-    actualizarBotonesEliminar();
+    const botonCopiar = document.createElement('button');
+    botonCopiar.id = 'boton-copiar';
+    botonCopiar.className = 'boton';
+    botonCopiar.textContent = 'Copiar al portapapeles';
+    botonCopiar.addEventListener('click', () => {
+      navigator.clipboard.writeText(carritoTexto);
+      alert('La información se ha copiado al portapapeles');
+    });
+    contenedorCarritoAcciones.insertBefore(botonCopiar, contenedorCarritoAcciones.firstChild);
+
+  } else {
+    contenedorCarritoVacio.classList.remove("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+  }
+
+  actualizarBotonesEliminar();
 }
 
 cargarProductosCarrito();
 
 function actualizarBotonesEliminar() {
-    botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+  botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 
-    botonesEliminar.forEach(boton => {
-        boton.addEventListener("click", eliminarDelCarrito)
-    });
+  botonesEliminar.forEach(boton => {
+    boton.addEventListener("click", eliminarDelCarrito)
+  });
 }
 
 function eliminarDelCarrito() {
